022910381920ae77b165dd1cb8b1d50795791593
Thiet Nguyen <nguyenthiet1225@gmail.com>
2022-01-06 06:18:42 +0700

    new pj

---
+++ 05_01_22_Operator_Exercise_2.js
@@ -0,0 +1,18 @@
+let x = prompt("What's your role");
+let y
+
+
+
+if (x == 'Employee'){
+    y = 'Hello';
+}
+else if(x == 'Director'){
+    y = 'Greetings';
+}
+else if (x == ''){
+    y = 'No login';
+}
+else{
+    y= ''
+}
+alert(y)
